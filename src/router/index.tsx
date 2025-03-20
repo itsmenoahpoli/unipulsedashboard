277f3f65413c6bbed47e210cb4fe6914775a706f
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-03-20 22:14:05 +0800

    update dashboard

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
+import { DashboardLayout } from "@/layouts";
 import { SigninPage } from "@/pages/SigninPage";
+import { Homepage } from "@/pages/Homepage";
 
 const router = createBrowserRouter([
   {
@@ -9,6 +11,16 @@ const router = createBrowserRouter([
   {
     path: "/signin",
     element: <SigninPage />,
+  },
+  {
+    path: "/admin",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/admin/home",
+        element: <Homepage />,
+      },
+    ],
   },
 ]);
 
